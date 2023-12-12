fcf2074558a9671a92f17414fc038698e0a4408d
Samala Kalyan Kumar Reddy <82137130+Samala-Kalyan-Kumar-Reddy@users.noreply.github.com>
2023-12-12 18:40:30 -0500

    Updated with Two more Approaches

--- src/main/java/twoPointers/longestSubArray.java
+++ src/main/java/twoPointers/longestSubArray.java
@@ -35,3 +35,77 @@ public class longestSubArray {
         System.out.println("The length of the longest subarray is: " + len);
     }
 }
+
+
+//Using two (Better Approach)
+
+/*
+public static int getLongestSubarray(int []a, int k) {
+        int n = a.length; // size of the array.
+
+        int len = 0;
+        for (int i = 0; i < n; i++) { // starting index
+            int s = 0;
+            for (int j = i; j < n; j++) { // ending index
+                // add the current element to
+                // the subarray a[i...j-1]:
+                s += a[j];
+
+                if (s == k)
+                    len = Math.max(len, j - i + 1);
+            }
+        }
+        return len;
+ */
+
+//Using HashMap(Optimal Approach and using two Pointers)
+
+/*
+
+
+
+import java.util.*;
+
+public class tUf {
+    public static int getLongestSubarray(int []a, int k) {
+        int n = a.length; // size of the array.
+
+        Map<Integer, Integer> preSumMap = new HashMap<>();
+        int sum = 0;
+        int maxLen = 0;
+        for (int i = 0; i < n; i++) {
+            //calculate the prefix sum till index i:
+            sum += a[i];
+
+            // if the sum = k, update the maxLen:
+            if (sum == k) {
+                maxLen = Math.max(maxLen, i + 1);
+            }
+
+            // calculate the sum of remaining part i.e. x-k:
+            int rem = sum - k;
+
+            //Calculate the length and update maxLen:
+            if (preSumMap.containsKey(rem)) {
+                int len = i - preSumMap.get(rem);
+                maxLen = Math.max(maxLen, len);
+            }
+
+            //Finally, update the map checking the conditions:
+            if (!preSumMap.containsKey(sum)) {
+                preSumMap.put(sum, i);
+            }
+        }
+
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        int[] a = { -1, 1, 1};
+        int k = 1;
+        int len = getLongestSubarray(a, k);
+        System.out.println("The length of the longest subarray is: " + len);
+    }
+}
+
+ */

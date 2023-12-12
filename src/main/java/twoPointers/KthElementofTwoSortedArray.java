154203e1275e89ff2c57cc7104b2261d5bf6d2ba
Samala Kalyan Kumar Reddy <82137130+Samala-Kalyan-Kumar-Reddy@users.noreply.github.com>
2023-12-11 23:11:12 -0500

    KthElementofTwoSortedArray

---
+++ src/main/java/twoPointers/KthElementofTwoSortedArray.java
@@ -0,0 +1,66 @@
+package twoPointers;
+
+/*
+Given two arrays arr1 and arr2 of size N and M respectively and an element K. The task is to find the element that would be at the kth position of the final sorted array.
+
+
+Example 1:
+
+Input:
+arr1[] = {2, 3, 6, 7, 9}
+arr2[] = {1, 4, 8, 10}
+k = 5
+Output:
+6
+Explanation:
+The final sorted array would be -
+1, 2, 3, 4, 6, 7, 8, 9, 10
+The 5th element of this array is 6.
+Example 2:
+Input:
+arr1[] = {100, 112, 256, 349, 770}
+arr2[] = {72, 86, 113, 119, 265, 445, 892}
+k = 7
+Output:
+256
+Explanation:
+Final sorted array is - 72, 86, 100, 112,
+113, 119, 256, 265, 349, 445, 770, 892
+7th element of this array is 256.
+ */
+
+public class KthElementofTwoSortedArray {
+
+        public long kthElement(int arr1[], int arr2[], int n, int m, int k) {
+            int i = 0, j = 0;
+
+            while (i < n && j < m) {
+                if (arr1[i] < arr2[j]) {
+                    if (++i == k) {
+                        return arr1[i - 1];
+                    }
+                } else {
+                    if (++j == k) {
+                        return arr2[j - 1];
+                    }
+                }
+            }
+
+            // If one of the arrays is exhausted, the kth element is in the other array
+            while (i < n) {
+                if (++i == k) {
+                    return arr1[i - 1];
+                }
+            }
+
+            while (j < m) {
+                if (++j == k) {
+                    return arr2[j - 1];
+                }
+            }
+
+            // If k is out of bounds (greater than the total number of elements)
+            return -1;
+        }
+    }
+
